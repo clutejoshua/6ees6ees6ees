80c78d9febc2d12c4d389dda1d7af81c0e054b38
clutejoshua <57841091+clutejoshua@users.noreply.github.com>
2020-03-30 11:23:27 -0700

    Finished Grub and Bee class using constructor() and super()

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,9 @@
-class Bee {
-  // TODO..
+class Bee extends Grub {
+  constructor() {
+    // Must call super constructor in derived class before accessing 'this' or returning from derived constructor
+    super();
+    this.age = 5;
+    this.color = 'yellow';
+    this.job = 'Keep on growing';
+  }
 };
